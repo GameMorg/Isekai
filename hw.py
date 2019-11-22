290ccfccffbf5d4f00cdc149d1d2a512c14b325e
GameMorg <mylisyn@gmail.com>
2019-11-22 19:04:49 +0300

    Add hw.py

--- hw.py
+++ hw.py
@@ -1,1 +1,16 @@
 print('hello world')
+a = int(input("Введите число а"))
+b = int(input("Введите число b"))
+
+sum = a + b
+if a > b:
+    max = a
+elif b > a:
+    max = b
+else:
+    max = "Числа равны"
+
+print("a =", a)
+print("b =", b)
+print("max =", max)
+print("sum =", sum)
