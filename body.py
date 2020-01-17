adfc096fbf359cd8fa7c7460062cf28cd294d1f0
GameMorg <mylisyn@gmail.com>
2020-01-17 19:30:29 +0300

    my first button!

--- body.py
+++ body.py
@@ -3,17 +3,18 @@ from save_game import *
 
 
 class Button():
-    def __init__(self, widht, height, ineractive_colar, active_color):
+    def __init__(self, widht, height, ineractive_colar=(23, 204, 58), active_color=(13, 162, 58)):
         self.widht = widht
         self.height = height
         self.ineractive_colar = ineractive_colar
         self.active_color = active_color
+
     def draw(self, x, y, message, win, action=None):
         mosue = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
         if x < mosue[0] < x + self.widht:
-            if y <  mosue[1] < y + self.height:
+            if y < mosue[1] < y + self.height:
                 pygame.draw.rect(win, (23, 204, 58), (x, y, self.widht, self.height))
 
                 if click[0] == 1 and action is not None:
@@ -22,6 +23,7 @@ class Button():
 
         else:
             pygame.draw.rect(win, (13, 162, 58), (x, y, self.widht, self.height))
+        print_text(message, x + 10, y + 10, win)
 
 
 class Hero(object):
@@ -46,10 +48,6 @@ class Camera(object):
     def __init__(self, arg):
         super(Camera, self).__init__()
         self.arg = arg
-
-
-def load_img():
-    pass
 
 
 def main():
@@ -95,6 +93,12 @@ def main():
     pygame.quit()
 
 
+def print_text(message, x, y, win, font_color=(0, 0, 0), font_type='font_type.otf', font_size=30):
+    font_type = pygame.font.Font(font_type, font_size)
+    text = font_type.render(message, True, font_color)
+    win.blit(text, (x, y))
+
+
 def menu():
     show = True
 
@@ -104,13 +108,12 @@ def menu():
 
     BLUE = (0, 0, 255)
 
-    meun_backr = pygame.image.load('//textures//backr.jpg')
-    start_bth = Button(300, 70)
+    meun_backr = pygame.image.load('textures//backr.jpg')
+    start_bth = Button(300, 70, )
 
     display_widht = 800
     display_height = 600
     win = pygame.display.set_mode((display_widht, display_height))
-
     while show:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -120,5 +123,6 @@ def menu():
                 if event.button == 1:
                     pass
 
-        win.plit(meun_backr, (0, 0))
+        win.blit(meun_backr, (0, 0))
+        start_bth.draw(100, 100, "my first button", win)
         pygame.display.update()
